32a153ad383a38f88d745301756b16c0b66bdb77
Santiago <santimh04@gmail.com>
2025-02-06 18:49:07 -0500

    Primeros pinitos del CRUD de casas

--- src/main/java/org/example/baseDatos/Conexion.java
+++ src/main/java/org/example/baseDatos/Conexion.java
@@ -3,11 +3,13 @@ package org.example.baseDatos;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.TimeZone;
 
 public class Conexion {
         //Atributos de Conexion
     /*private static String URL = "jdbc:mysql://<hostname>:<port>/<database>?serverTimezone=America/Bogota";*/  //Los elementos en <> se deben cambiar, incluyendo al operador diamante
-    private static String URL = "jdbc:mysql://localhost:3306/estancias_exterior?serverTimezone=America/Bogota";
+    private static String serverTimezone = TimeZone.getDefault().getID();   //Aquí pega la zona horaria localmente
+    private static String URL = ("jdbc:mysql://localhost:3306/estancias_exterior?serverTimezone=").concat(serverTimezone);
     private static String USERNAME = "root";
     private static String PASSWORD = "sasa";
     private static java.sql.Connection Connection;
@@ -18,7 +20,7 @@ public class Conexion {
     //Lectores de atributos de Conexion (getter)
         //Métodos de Conexion
     public static Connection getInstance() throws SQLException {
-        if(Connection == null){
+        if(Connection == null){/**/
             Connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         }
         return Connection;
--- src/main/java/org/example/persistencia/implementaciones/CasaDAO.java
+++ src/main/java/org/example/persistencia/implementaciones/CasaDAO.java
@@ -1,8 +1,11 @@
 package org.example.persistencia.implementaciones;
 
+import org.example.baseDatos.Conexion;
 import org.example.entidades.Casa;
 import org.example.persistencia.CRUD_DAO;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,23 +15,103 @@ public class CasaDAO implements CRUD_DAO<Casa> {
     //Asignadores de atributos de CasaDAO (setters)
     //Lectores de atributos de CasaDAO (getters)
         //Métodos de CasaDAO
+    private Connection getConnection() throws SQLException {
+        return Conexion.getInstance();
+    }
+
     @Override
     public List<Casa> listar() {
-        return List.of();
+        List<Casa> casas = new ArrayList<>();
+            //Obtener la conexión
+        try (Connection Conn = getConnection();
+             Statement Stmt = Conn.createStatement();
+             ResultSet RS = Stmt.executeQuery("SELECT * from casas")){
+                //Recorrer el cursor
+            while(RS.next()){
+                Casa casa = crearCasa(RS);
+                    //Poblar la lista
+                casas.add(casa);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return casas;
     }
 
     @Override
     public Optional<Casa> porID(Long id) {
-        return Optional.empty();
+        Casa casa = null;
+
+        try (Connection Conn = getConnection();
+             PreparedStatement PS = Conn.prepareStatement("SELECT c.* from casas where c.id=?")){
+            PS.setLong(1, id);
+            //Ejecutar la consulta
+            try (ResultSet RS = PS.executeQuery()) {
+                //Recorrer el cursor
+                if (RS.next()) {
+                        //Crear objeto de tipo Producto
+                    casa = crearCasa(RS);
+                }
+            }
+        }  catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.ofNullable(casa);
     }
 
     @Override
     public void guardar(Casa casa) {
+            //Preparar parámetros
+        String SQL;
+        if (casa.getIdCasa() != null && casa.getIdCasa() > 0) {
+            SQL = "UPDATE casas SET fecha_desde = ?, fecha_hasta = ?, precio_habitacion = ? WHERE id_casa = ?";
+        } else {
+            SQL = "INSERT INTO productos (fecha_desde, fecha_hasta, precio_habitacion, calle, numero, codigo_postal, ciudad, pais, tipo_vivienda) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        }
+            //Obtener la conexión
+        try (Connection Conn = getConnection();
+             PreparedStatement PS= Conn.prepareStatement(SQL)){
+                //Asignar parámetros
+            PS.setDate(1, Date.valueOf(casa.getFechaDesde()));
+            PS.setDate(2, Date.valueOf(casa.getFechaHasta()));
+            PS.setFloat(3, casa.getPrecioHabitacion());
 
+            if (casa.getIdCasa() != null && casa.getIdCasa() > 0){
+                PS.setLong(4, casa.getIdCasa());    //Actualizar la tabla
+            } else {
+                PS.setString(4, casa.getCalle());
+                PS.setInt(5, casa.getNumero());
+                PS.setString(6, casa.getCodigoPostal());
+                PS.setString(7, casa.getCiudad());
+                PS.setString(8, casa.getPais());
+                PS.setString(9, casa.getTipoVivienda());
+            }
+                //Actualizar la base de datos
+            PS.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void eliminar(Long id) {
 
+    }
+
+    private static Casa crearCasa(ResultSet RS) throws SQLException {
+        Casa c = new Casa();
+            c.setIdCasa(RS.getLong("id_casa"));
+            c.setCalle(RS.getString("calle"));
+            c.setNumero(RS.getInt("numero"));
+            c.setCodigoPostal(RS.getString("codigo_postal"));
+            c.setCiudad(RS.getString("ciudad"));
+            c.setPais(RS.getString("pais"));
+            c.setFechaDesde(RS.getDate("fecha_desde").toLocalDate());
+            c.setFechaHasta(RS.getDate("fecha_hasta").toLocalDate());
+            c.setTiempoMinimo(RS.getInt("tiempo_minimo"));
+            c.setTiempoMaximo(RS.getInt("tiempo_maximo"));
+            c.setPrecioHabitacion(RS.getFloat("precio_habitacion"));
+            c.setTipoVivienda(RS.getString("tipo_vivienda"));
+        return c;
     }
 }
